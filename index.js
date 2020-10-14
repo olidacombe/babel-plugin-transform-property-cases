0185e29e824c7bc9237d2a9030704c3a547c97b0
Oli <oliver@fraclog.co>
2020-10-14 23:11:49 +0100

    might as well make it more general

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const { paramCase } = require('change-case');
 const transformRename = require('babel-plugin-transform-rename-properties');
 
 module.exports = function ({ types: t }, options = {}) {
-  const kebabize = options.kebabize || [];
-  const rename = kebabize.reduce((acc, v)=>({...acc, [v]: paramCase(v)}), {});
+  const rename = {};
+  const mappings = [['kebabCase', paramCase]];
+  for(const [key, f] of mappings) {
+      const targets = options[key] || [];
+      Object.assign(rename, targets.reduce((acc, v)=>({...acc, [v]: f(v)}), {}));
+  }
 
   return transformRename({types: t}, {rename});
 }
