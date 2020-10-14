d118c5eed57b3caa68d30077823bc5747b1f8c7f
Oli <oliver@fraclog.co>
2020-10-14 23:15:00 +0100

    prettier

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const { paramCase } = require('change-case');
-const transformRename = require('babel-plugin-transform-rename-properties');
+const { paramCase } = require("change-case");
+const transformRename = require("babel-plugin-transform-rename-properties");
 
 module.exports = function ({ types: t }, options = {}) {
   const rename = {};
-  const mappings = [['kebabCase', paramCase]];
-  for(const [key, f] of mappings) {
-      const targets = options[key] || [];
-      Object.assign(rename, targets.reduce((acc, v)=>({...acc, [v]: f(v)}), {}));
+  const mappings = [
+    ["kebabCase", paramCase],
+    ["paramCase", paramCase],
+  ];
+  for (const [key, f] of mappings) {
+    const targets = options[key] || [];
+    Object.assign(
+      rename,
+      targets.reduce((acc, v) => ({ ...acc, [v]: f(v) }), {})
+    );
   }
 
-  return transformRename({types: t}, {rename});
-}
+  return transformRename({ types: t }, { rename });
+};
