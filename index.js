6da6c8aa9b0b7466cc89f9d0c94e83cc9de9374f
Oli <oliver@fraclog.co>
2020-10-15 11:50:09 +0100

    testability factor

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const { paramCase } = require("change-case");
 const transformRename = require("babel-plugin-transform-rename-properties");
 
-module.exports = function ({ types: t }, options = {}) {
-  const rename = {};
-  const mappings = [
+function renameOptionsFromCasesOptions(options = {}) {
+  const renameOptions = {};
+  const caseTransforms = [
     ["kebabCase", paramCase],
     ["paramCase", paramCase],
   ];
-  for (const [key, f] of mappings) {
+  for (const [key, f] of caseTransforms) {
     const targets = options[key] || [];
     Object.assign(
-      rename,
+      renameOptions,
       targets.reduce((acc, v) => ({ ...acc, [v]: f(v) }), {})
     );
   }
+  return renameOptions;
+}
 
+module.exports = function ({ types: t }, options = {}) {
+  const rename = renameOptionsFromCasesOptions(options);
   return transformRename({ types: t }, { rename });
 };
